d24af45e36e32dfa7771ad2d29983dabeeccd984
Hannah Devlin <hannahdevlin@hannahs-mbp.home>
2021-11-28 18:23:17 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+// Component declaration
+//  - selector: tag you'll use to embed your component
+//  - templateUrl: HTML file your using
+//  - styleUrls: css file
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-initial';
-}
+// class contains any props & methods of the component
+export class AppComponent {}
